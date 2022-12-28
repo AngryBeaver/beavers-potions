b0e481c3806abe17ae3ce101ba9a457e05273c11
AngryBeaver <github@verplanmich.net>
2022-12-28 12:00:54 +0100

    add potions.db

---
+++ src/EffectHooks.ts
@@ -0,0 +1,30 @@
+export function distortion(workflow, token){
+    const random = Math.floor(Math.random() * 5)+1;
+    if(random === 5){
+        workflow.targets.delete(token);
+    }
+}
+
+export function reflectDamage(actor, damageItem){
+    let damage = damageItem.appliedDamage;
+    if(damage > 0 && actor.system.attributes.hp.temp > 0){
+        let tempHP = Math.max(actor.system.attributes.hp.temp-damage,0);
+        damage = Math.max(damage-actor.system.attributes.hp.temp,0);
+        actor.update({
+            "system.attributes.hp.temp": tempHP
+        }).then(data => console.log(data));
+    }
+    if(damage > 0 && actor.system.attributes.hp.temp > 0){
+        let value = Math.max(actor.system.attributes.hp.value-damage,0);
+        actor.update({
+            "system.attributes.hp.value":value
+        }).then(data=>console.log(data));
+    }
+}
+
+export function protectionFromDeath(actor,effect){
+    window.setTimeout(()=>{
+        effect.delete();
+        actor.update({system:{attributes:{hp:{value:1}}}});
+    },1000);
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,31 @@
+import {distortion, protectionFromDeath, reflectDamage} from "./EffectHooks.js";
+
+
+
 Hooks.on("midi-qol.preCheckHits",(workflow)=>{
     workflow.targets.forEach(token=>{
-        token.documents.actor.effects.forEach((key,value)=>{
-           if(value.label === "Distortion"){
-               const random = Math.floor(Math.random() * 5)+1;
-               if(random === 5 || true){
-                   workflow.targets.delete(token);
-               }
+        token.document.actor.effects.forEach((effect)=>{
+           if(effect.label === "Distortion"){
+               distortion(workflow, token)
            }
         });
     })
 });
 
+Hooks.on("midi-qol.damageApplied",(token,next)=>{
+    token.document.actor.effects.forEach((effect)=>{
+       if(effect.label === "Reflect Damage"){
+           reflectDamage(next.workflow.actor, next.ditem)
+       }
+    });
+});
+
+Hooks.on("updateActor",(actor,update,data,id)=>{
+    if(update.system?.attributes?.hp?.value <= 0 ){
+        actor.effects.forEach((effect)=>{
+            if(effect.label === "Protection From Death"){
+                protectionFromDeath(actor,effect);
+            }
+        });
+    }
+});
